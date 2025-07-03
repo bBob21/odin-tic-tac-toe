d4e25ec7b42c8397a0a34c8be925919f44f53ce4
bBob21 <102711034+bBob21@users.noreply.github.com>
2025-07-03 16:03:50 +1000

    Added restart feature

--- script.js
+++ script.js
@@ -20,6 +20,14 @@ function createPlayer(symbol, game){
 
 const game = (function (){
     const board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
+    function resetBoard() {
+        for (let r = 0; r < board.length; r++){
+            for (let c = 0; c < board[r].length; c++){
+                board[r][c] = ' ';
+            }
+        }
+        console.log(board)
+    }
     function checkStatus(){
         // TODO fix this ugly af
         for (let r = 0; r < board.length; r++){
@@ -41,77 +49,78 @@ const game = (function (){
         if (!board.flat().includes(' ')){
             return "draw";
         }
-        return false
+        return false;
     }
     function showBoard(){
         for (let r = 0; r < board.length; r++){
-            console.log(board[r])
+            console.log(board[r]);
         }
     }
-    return {board, checkStatus, showBoard};
+
+    function renderBoard(){
+        const domBoard = document.getElementById("board");
+        domBoard.replaceChildren();
+        const domWinMessage = document.querySelector(".winMessage");
+        domWinMessage.textContent = "";
+
+        let currPlayer = 0;
+        for (let r = 0; r < ROWS; r++){
+            for (let c = 0; c < COLUMNS; c++){
+                const cell = document.createElement("button")
+                cell.classList.add("cell");
+                cell.dataset.row = r;
+                cell.dataset.col = c;
+
+                cell.addEventListener("click",() =>{
+                    console.log(`row ${cell.dataset.row}`);
+                    console.log(`col ${cell.dataset.col}`);
+                    
+                    let p = players[currPlayer]
+                    let validTurn = p.takeTurn([cell.dataset.row, cell.dataset.col]) 
+                    if (validTurn){
+                        cell.textContent = players[currPlayer].symbol;
+                        if (currPlayer == 0){
+                            currPlayer++;
+                        }
+                        else{
+                            currPlayer--;
+                        }
+                    }
+                    winStatus = game.checkStatus();
+                    if (!!winStatus){
+                        gameRun = false;
+                        if (winStatus == "draw"){
+                            domWinMessage.textContent = "Draw, noone wins"
+                            console.log("Draw")
+                        }
+                        else{
+                            domWinMessage.textContent = `${winStatus} wins`;
+                            console.log(`${winStatus} wins`);
+                        }
+                        let cells = document.querySelectorAll(".cell");
+                        cells.forEach(cell => {
+                            cell.disabled = true;
+                        })
+                    }
+                });
+                domBoard.appendChild(cell);
+            }
+        }
+    }
+    return {board, checkStatus, showBoard, renderBoard, resetBoard};
 })();
 
 // ----------------------------------------
+const domResetBtn = document.querySelector(".resetBtn");
+domResetBtn.addEventListener("click", () => {
+    game.resetBoard();
+    game.renderBoard();
+})
 const player1 = createPlayer("X", game);
 const player2 = createPlayer("O", game);
 const players = [player1, player2]
 
 const ROWS = 3;
 const COLUMNS = 3;
-const domBoard = document.getElementById("board");
-const domWinMessage = document.querySelector(".winMessage");
 
-const observer = new MutationObserver(() =>{
-    console.log("Disabled board");
-    let cells = document.querySelectorAll(".cell");
-    cells.forEach(cell => {
-        cell.disabled = true;
-    })
-})
-
-observer.observe(domWinMessage, {
-    characterData:true,
-    subtree:true,
-    childList:true,
-})
-
-let currPlayer = 0;
-for (let r = 0; r < ROWS; r++){
-    for (let c = 0; c < COLUMNS; c++){
-        const cell = document.createElement("button")
-        cell.classList.add("cell");
-        cell.dataset.row = r;
-        cell.dataset.col = c;
-
-        cell.addEventListener("click",() =>{
-            console.log(`row ${cell.dataset.row}`);
-            console.log(`col ${cell.dataset.col}`);
-            
-            let p = players[currPlayer]
-            let validTurn = p.takeTurn([cell.dataset.row, cell.dataset.col]) 
-            if (validTurn){
-                cell.textContent = players[currPlayer].symbol;
-                if (currPlayer == 0){
-                    currPlayer++;
-                }
-                else{
-                    currPlayer--;
-                }
-            }
-            winStatus = game.checkStatus();
-            if (!!winStatus){
-                gameRun = false;
-                if (winStatus == "draw"){
-                    domWinMessage.textContent = "Draw, noone wins"
-                    console.log("Draw")
-                }
-                else{
-                domWinMessage.textContent = `${winStatus} wins`;
-                console.log(`${winStatus} wins`);
-                }
-            }
-        });
-        domBoard.appendChild(cell);
-    }
-}
-
+game.renderBoard();
