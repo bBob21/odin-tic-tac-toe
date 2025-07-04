0507b71275e2a227ada3c9cb53920dc8f174c3a7
bBob21 <102711034+bBob21@users.noreply.github.com>
2025-07-04 15:40:43 +1000

    Changed structure to class

--- script.js
+++ script.js
@@ -1,60 +1,74 @@
-function createPlayer(symbol, game){
-    function logTurn(validAttempt, row, col){
-        console.log(`Attempt ${validAttempt}: ${symbol} at row ${row} column ${col}`)
-        game.showBoard()
+class Player{
+    constructor(symbol, game){
+        this._symbol = symbol;
+        this.game = game;
     }
-    function takeTurn([row,col]){
-        board = game.board;
-        validAttempt = true;
+    get symbol(){
+        return this._symbol;
+    }
+    set symbol(s){
+        this._symbol = s;
+    }
+
+    #logTurn = (validAttempt, row, col) => {
+        console.log(`Attempt ${validAttempt}: ${this.symbol} at row ${row} column ${col}`)
+        this.game.showBoard()
+    }
+    takeTurn = ([row,col]) => {
+        board = this.game.board;
+        let validAttempt = true;
         if (board[row][col] === ' '){
-            board[row][col] = symbol;
+            board[row][col] = this.symbol;
         }
         else{
             validAttempt = false;
         }
-        logTurn(validAttempt, row, col)
+        this.#logTurn(validAttempt, row, col)
         return validAttempt;
     }
-    return {symbol, takeTurn}
 }
 
-const game = (function (){
-    const board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
-    function resetBoard() {
-        for (let r = 0; r < board.length; r++){
-            for (let c = 0; c < board[r].length; c++){
-                board[r][c] = ' ';
+const game = new (class Game{
+    constructor(){
+        this.board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
+    }
+
+    resetBoard = () => {
+        for (let r = 0; r < this.board.length; r++){
+            for (let c = 0; c < this.board[r].length; c++){
+                this.board[r][c] = ' ';
             }
         }
         console.log(board)
     }
-    function setWinColor(coordinates){
+    setWinColor(coordinates){
         console.log(coordinates)
         for (let coord of coordinates){
             let winningCell = document.querySelector(`[data-row="${coord[0]}"][data-col="${coord[1]}"]`)
             winningCell.classList.add("winningCell");
         }
     }
-    function checkStatus(){
+    checkStatus = () => {
         // TODO fix this ugly af
+        let board = this.board;
         for (let r = 0; r < board.length; r++){
             if (board[r][0] === board[r][1] && board[r][1] === board[r][2] && board[r][2] !== ' '){
-                setWinColor([[r,0],[r,1],[r,2]])
+                this.setWinColor([[r,0],[r,1],[r,2]])
                 return board[r][0];
             }
         }
         for (let c = 0; c < board[0].length; c++){
             if (board[0][c] === board[1][c] && board[1][c] === board[2][c] && board[2][c] !== ' '){
-                setWinColor([[0,c],[1,c],[2,c]])
+                this.setWinColor([[0,c],[1,c],[2,c]])
                 return board[0][c];
             }
         }
         if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[2][2] !== ' '){
-            setWinColor([[0,0],[1,1],[2,2]])
+            this.setWinColor([[0,0],[1,1],[2,2]])
             return board[1][1];
         }
         if (board[2][0] === board[1][1] && board[1][1] === board[0][2] && board[0][2] !== ' '){
-            setWinColor([[2,0],[1,1],[0,2]])
+            this.setWinColor([[2,0],[1,1],[0,2]])
             return board[1][1]
         }
         if (!board.flat().includes(' ')){
@@ -62,13 +76,13 @@ const game = (function (){
         }
         return false;
     }
-    function showBoard(){
-        for (let r = 0; r < board.length; r++){
-            console.log(board[r]);
+    showBoard = () => {
+        for (let r = 0; r < this.board.length; r++){
+            console.log(this.board[r]);
         }
     }
 
-    function renderBoard(){
+    renderBoard = () =>{
         const domBoard = document.getElementById("board");
         domBoard.replaceChildren();
         const domGameStatus = document.querySelector(".gameStatus");
@@ -99,7 +113,7 @@ const game = (function (){
                     }
                     domGameStatus.textContent = `${PLAYERS[currPlayer].symbol}'s turn`;
 
-                    winStatus = game.checkStatus();
+                    let winStatus = this.checkStatus();
                     if (!!winStatus){
                         if (winStatus == "draw"){
                             domGameStatus.textContent = "Draw, noone wins"
@@ -119,8 +133,7 @@ const game = (function (){
             }
         }
     }
-    return {board, checkStatus, showBoard, renderBoard, resetBoard};
-})();
+});
 
 // ----------------------------------------
 const domResetBtn = document.querySelector(".resetBtn");
@@ -128,8 +141,8 @@ domResetBtn.addEventListener("click", () => {
     game.resetBoard();
     game.renderBoard();
 })
-const player1 = createPlayer("X", game);
-const player2 = createPlayer("O", game);
+const player1 = new Player("X", game);
+const player2 = new Player("O", game);
 const PLAYERS = [player1, player2]
 
 const ROWS = 3;
